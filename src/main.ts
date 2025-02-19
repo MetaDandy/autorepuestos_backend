4405e90752d89bb7963f1048920d084a35cedb72
MetaDandy <benitezarroyojoseph@gmail.com>
2025-02-19 16:38:59 -0400

    feat(core): helmet y rate limit implementado

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './guard/jwt-auth/jwt-auth.guard';
 import { PermissionGuard } from './guard/permission/permission.guard';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error', 'warn'] });
@@ -10,8 +12,17 @@ async function bootstrap() {
   app.useGlobalGuards(
     new JwtAuthGuard(reflector),
     new PermissionGuard(reflector)
-  )
-  
+  );
+
+  app.use(helmet);
+  app.use(rateLimit(
+    {
+      windowMs: 1000, // 1 segundo
+      max: 3, // Máximo 3 peticiones por IP en 1 segundo
+      message: 'Demasiadas solicitudes, espera un momento.',
+    }
+  ));
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(process.env.NODE_ENV)
 }
